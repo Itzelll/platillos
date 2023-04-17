c387e520a3a21c68bec765cf58b800c5dcde4908
Itzell <mmitzel2435@gmail.com>
2023-04-17 14:24:09 -0600

    update okteto

--- platillos/urls.py
+++ platillos/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    url(r'^ht/', include('health_check.urls')),
 ]
