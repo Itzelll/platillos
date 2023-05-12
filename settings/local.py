d736fa186728878534991cb5de7380982a2d2870
itzelll <mmitzel2435@gmail.com>
2023-05-12 11:11:17 -0600

    create authentication

--- settings/local.py
+++ settings/local.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'platillos.urls'
@@ -79,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'platillos_local',
-        'USER': 'postgres',
+        'USER': 'itzelll',
         'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '5432'
@@ -127,4 +128,12 @@ STATIC_URL = '/static/'
 
 GRAPHENE = {
     'SCHEMA': 'platillos.schema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
 }
+
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
