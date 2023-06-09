d20382e95524f8d394780fb342d563c7f9f2d3b5
itzelll <mmitzel2435@gmail.com>
2023-06-09 14:23:01 -0600

    update

--- myapp/schema.py
+++ myapp/schema.py
@@ -8,9 +8,17 @@ class DataEntryType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_data_entries = graphene.List(DataEntryType)
+    data_entries_by_user = graphene.List(DataEntryType, user=graphene.String(required=True))
+    data_entries_by_model = graphene.List(DataEntryType, model=graphene.String(required=True))
 
     def resolve_all_data_entries(self, info):
         return DataEntry.objects.all()
+    
+    def resolve_data_entries_by_user(self, info, user):
+        return DataEntry.objects.filter(user=user)
+    
+    def resolve_data_entries_by_model(self, info, model):
+        return DataEntry.objects.filter(model=model)
     
 
 class CreateDataEntry(graphene.Mutation):
